48f352c6434b0ac86ebd31903e3ce51267b198ae
SElnara01 <el.sadixova97@gmail.com>
2024-07-12 03:34:27 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,88 @@
+// ЗАДАНИЕ 1
+
+// let name1 = prompt('What is your name?');
+// alert('Привет,' + name1);
+
+// ----------------------------------------------------------------------------
+
+// ЗАДАНИЕ 2
+
+// let year = +prompt("Year of your birth");
+// const now = 2024;
+// if (year > now) {
+//   alert("LIAR");
+// } else {
+//   alert("You are" + " " + (now - year));
+// }
+
+// ----------------------------------------------------------------------------
+
+// ЗАДАНИЕ 3
+
+// let length = +prompt('Enter a length');
+// alert(4 * length);
+
+// ----------------------------------------------------------------------------
+
+// ЗАДАНИЕ 4
+
+// let radius = +prompt('Enter a number');
+// const pi = 3.14;
+// alert(pi * radius * radius);
+
+// ----------------------------------------------------------------------------
+
+// ЗАДАНИЕ 5
+
+// let s = +prompt('Enter a distance');
+// let t = +prompt('Enter time');
+// let v = s/t;
+// alert(v);
+
+// ----------------------------------------------------------------------------
+
+// ЗАДАНИЕ 6
+
+// let dollar = +prompt('Enter a number');
+// const exchange = 0.92
+// alert(dollar * exchange);
+
+// ----------------------------------------------------------------------------
+
+// ЗАДАНИЕ 7
+
+// let volume1 = +prompt('Enter a number');
+// const volume2 = 0.82
+// let x = volume1/volume2;
+// alert(Math.floor(x));
+
+// ----------------------------------------------------------------------------
+
+// ЗАДАНИЕ 8
+
+// let sum = +prompt('Enter the amount of money');
+// let price = +prompt('Enter the price');
+// let quantity = sum / price;
+// let change = sum % price;
+// alert('Number of chocolate =' + ' ' + Math.floor(quantity) + '\n' + 'Your change =' + ' ' + change + ' AZN');
+
+// ----------------------------------------------------------------------------
+
+// ЗАДАНИЕ 9
+
+// let number1 = +prompt('Enter a number');
+// let firstNumeral = number1%10;
+// let secondNumeral = Math.floor(number1/10) % 10;
+// let thirdNumeral = Math.floor(number1/100) % 10;;
+// let number2 = (firstNumeral.toString() + secondNumeral.toString()  + thirdNumeral.toString());
+// alert(number2);
+
+// ----------------------------------------------------------------------------
+
+// ЗАДАНИЕ 10
+
+// let number = prompt('Enter a number');
+// let a = number % 2 == 0;
+// alert(a);
+// let b = number % 2 == 1;
+// alert(b);
